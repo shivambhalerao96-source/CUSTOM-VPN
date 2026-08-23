986acd85a21da429d03d43578d47ee1384c134fe
adwad343 <advaitwadke2@gmail.com>
2026-08-23 14:52:32 +0530

    added multiclient structure

--- client/transport.h
+++ client/transport.h
@@ -2,8 +2,11 @@
 #define TRANSPORT_H
 
 #include <arpa/inet.h>
-
+#include <string>
+using namespace std;
 void tunToServer(int tun_fd, int sockfd, sockaddr_in serverAddress);
+string receiveHandshake(int sockfd);
+int sendHandshake(int sockfd, sockaddr_in serverAddress);
 
 void serverToTun(int tun_fd, int sockfd);
 
--- tun/setup.h
+++ tun/setup.h
@@ -3,7 +3,7 @@
 
 int up();
 int assign_ipaddress();
-int create_tun_interface();
+int create_tun_interface(string vpn_ip);
 void reroute();
 void close_tun();
 
